39a3a7d624a026dec441926939689cb7fdf71f5e
JenVest2020 <jenvest2019@gmail.com>
2020-07-31 20:28:24 -0500

    installed npm & dependencies/ created reducer

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    isPosting: false,
+    error: ''
+};
+
+export const smurfReducer = (state = initialState, action) => {
+
+    return state
+};
+
