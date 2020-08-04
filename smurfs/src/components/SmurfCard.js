eda849754d9ef898114c3d37294dbb011acdf823
JenVest2020 <jenvest2019@gmail.com>
2020-08-03 20:31:51 -0500

    working on testing for stretch

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 
 const SmurfCard = (props) => {
-
+    const image = require('../components/Shroom.png');
     return (
         <div className='App-div'>
+            <img className='image' src={image} alt='red mushroom with white polka dots' />
+        <div className= 'text'>
             <h1>Name: {props.smurf.name}</h1>
             <h3>Age: {props.smurf.age}</h3>
             <h3>Height: {props.smurf.height}</h3>
+        </div>
         </div>
     )
 };
--- smurfs/src/setupTests.js
+++ smurfs/src/setupTests.js
@@ -3,7 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
+import React from "react";
+import ReactDOM from 'react-dom';
+import { render, fireEvent } from "@testing-library/react";
 import SmurfForm from './components/SmurfForm';
 
 test('test form inputs', async () => {
@@ -14,4 +16,20 @@ test('test form inputs', async () => {
         target: { value: 'Sleepy' }
     });
     expect(getByTestId(Name)).toHaveValue('Sleepy');
+
+    const Age = getByTestId('age');
+    fireEvent.change(Age, {
+        target: { value: '500' }
+    });
+    expect(getByTestId(Age)).toHaveValue('500');
+
+    const Height = getByTestId('height');
+    fireEvent.change(Height, {
+        target: { value: '5CM' }
+    });
+    expect(getByTestId(Height)).toHaveValue('5CM');
+
+    const button = getByTestId('submit');
+    fireEvent.click(button)
 })
+
