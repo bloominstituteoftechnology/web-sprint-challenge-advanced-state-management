d30e7b736e1237362ff2bf1b8bc0390beb78af34
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-28 18:23:39 -0500

    imported createStore, applyMiddleware, Provider, and thunk in src/index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 ReactDOM.render(<App />, document.getElementById("root"));
