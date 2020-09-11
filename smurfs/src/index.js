778d68d604629e28948eb9849083d11f65e9883c
joel vega <joelvega97@yahoo.com>
2020-09-11 12:04:05 -0400

    Set up the files.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux'
+import {applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+
+import reducer from './store/reducers'
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+const store = createStore(reducer, applyMiddleware(logger))
+
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>, document.getElementById("root"));
---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,15 @@
+const initalState = {
+    smurfData: [
+        {name:"Brainey",
+        age:200,
+        height:"5cm",
+        id:0}
+    ]
+}
+
+export default (state = initalState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
