dc649593dc196d77d07f548ccdf6aa772fd24105
Samantha Goodman <slgoodman89@gmail.com>
2020-09-25 22:01:10 -0400

    set up boilplate and can fetch data and get state :)

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import {connect} from 'react-redux';
+
+import {getSmurfData} from '../actions'
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+
+const App = (props) => {
+  useEffect(() => { 
+    props.getSmurfData()
+  }, [])   
+
+  return (
       <div className="App">
         <h1>SMURFS! W/Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
@@ -10,7 +17,13 @@ class App extends Component {
         <div>Have fun!</div>
       </div>
     );
+}
+
+const mapStatetoProps = (state) => {
+  return {
+    smurfData:  state.smurfData,
+    isLoading:  state.isLoading,
   }
 }
 
-export default App;
+export default connect(mapStatetoProps, {getSmurfData})(App);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {reducer} from './reducers'
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store = { store }><App /></Provider>, document.getElementById("root"));
