23c10b3c5ebad730b0774e0efd497e064bb4512e
Samantha Goodman <slgoodman89@gmail.com>
2020-09-25 21:30:52 -0400

    created actions to get smurf data

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const ACTIONS = {
+    AXIOS_GET_START: 'AXIOS_GET_START',
+    AXIOS_GET_SUCCESS: 'AXIOS_GET_SUCCES',
+    AXIOS_GET_FAIL: 'AXIOS_GET_FAIL'
+}
+
+export const getSmurfData = () => (dispatch) => {
+    dispatch({
+        type: ACTIONS.AXIOS_GET_START
+    })
+    setTimeout(() => {
+        axios.get('http://localhost:3333/smurfs/')
+            .then(res => {
+                console.log('AXIOS SUCESS', res)
+                dispatch({
+                    type:ACTIONS.AXIOS_GET_SUCCESS,
+                    payload: res.data
+                })
+            })
+            .catch(err => {
+                console.log('AXIOS ERROR', err)
+                dispatch({
+                    type: ACTIONS.AXIOS_GET_FAIL,
+                    payload: err.message
+                })
+            })
+    }, 3000)
+ 
+}
