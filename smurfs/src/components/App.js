10d10fc8e0007e27b96de3a429a5efca5ec07981
Ryan Clark <rbclark244@gmail.com>
2020-09-28 18:23:05 -0400

    json display looks terrible but this MIGHT be MVP

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,56 +1,112 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import { connect } from "react-redux";
 import { getData } from "../actions/index";
+// import Display from "./Display";
 
 const App = (props) => {
+  const [smurf, setSmurf] = useState({ name: "", age: "", height: "" });
+  const [post, setPost] = useState();
+
   useEffect(() => {
     props.getData();
   }, []);
 
+  const handleChanges = (e) => {
+    setSmurf({ ...smurf, [e.target.name]: e.target.value });
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then((res) => {
+        console.log(res.data);
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
-      <form>
-        <label>
-          New Smurf:
-          <input type="text" placeholder="name" />
-          <input type="text" placeholder="age" />
-          <input type="text" placeholder="height" />
+      <div className="Form">
+        <form onSubmit={formSubmit}>
+          <label>New Smurf:</label>
+          <input
+            name="name"
+            id="name"
+            value={smurf.name}
+            type="text"
+            placeholder="name"
+            onChange={handleChanges}
+          />
+          <input
+            name="age"
+            id="age"
+            value={smurf.age}
+            type="text"
+            placeholder="age"
+            onChange={handleChanges}
+          />
+          <input
+            name="height"
+            id="height"
+            value={smurf.height}
+            type="text"
+            placeholder="height"
+            onChange={handleChanges}
+          />
           <button>Submit</button>
-        </label>
-      </form>
-      <div className="display"></div>
+        </form>
+        <div className="display"></div>
+      </div>
+
+      <pre>{JSON.stringify(post, null, 2)}</pre>
+      {/* <Display smurf={smurf} /> */}
     </div>
   );
 };
 
-const testData = { name: "Rob", age: 2, height: "massive" };
-
-// const formSubmit = (e) => {
-//   e.preventDefault();
-//   axios
-//     .post("http://localhost:3333/smurfs", testData)
-//     .then((res) => {
-//       console.log(res.data);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
-
 const mapStateToProps = (state) => {
+  // console.log(state.smurfData);
   return {
     smurfData: state.smurfData,
     is_loading_data: state.is_loading_data,
     error: state.error,
   };
 };
-
 export default connect(mapStateToProps, { getData })(App);
 
-{
-  /* <h1>{props.smurfData.name}</h1>
+/* <div className="display">
+{smurf.map((smurfs) => {
+  <div>
+    <h1>{smurfs}</h1>
+    {/* <h2>{smurfs.age}</h2>
+    <h2>{smurfs.height}</h2> */
+// })}
+// </div> */}
+
+// {
+//   const handleChanges = (e) => {
+//     const [smurf, setSmurf] = useState();
+//     setSmurf({ ...smurf, [e.target.name]: e.target.value });
+//   };
+
+//   const formSubmit = (e) => {
+//     e.preventDefault();
+//     axios
+//       .post("http://localhost:3333/smurfs", smurf)
+//       .then((res) => {
+//         console.log(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+/* <h1>{props.smurfData.name}</h1>
       {props.is_loading_data ? (
         <div> Loading... </div>
       ) : props.error ? (
@@ -61,4 +117,5 @@ export default connect(mapStateToProps, { getData })(App);
           <p>{props.smurfData.height}</p>
         </>
       )} */
-}
+
+// const testData = { name: "Rob", age: 2, height: "massive" }
---
+++ smurfs/src/components/Display.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// const Display = (props) => {
+//   console.log(props.smurf);
+//   //   smurf.map((smurfs) => {
+//   return (
+//     <div className="display">
+//       {/* <h1>{smurfs}</h1>
+//         <h2>{smurfs.age}</h2>
+//         <h2>{smurfs.height}</h2> */}
+//     </div>
+//   );
+//   //   });
+// };
+
+// export default Display;
