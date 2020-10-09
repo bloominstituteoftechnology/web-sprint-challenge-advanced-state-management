bd7fab531f6adf809768fa15247df2e0871aab68
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-09 10:19:20 -0600

    Form and List added to App

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,62 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! W/Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+// import React, { Component } from "react";
+// import "./App.css";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>SMURFS! W/Redux</h1>
+//         <div>Welcome to your state management version of Smurfs!</div>
+//         <div>Start inside of your `src/index.js` file!</div>
+//         <div>Have fun!</div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from "react";
+import { connect } from "react-redux";
+import SmurfList from "./SmurfList";
+import SmurfForm from "./SmurfForm";
+import { getSmurf } from "../actions/index";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+/* styles */
+  margin: 40px;
+  border: 10px white;
+  background: papayawhip;
+  color: #4f4f4f;
+  `;
+
+const Header = styled.h1`
+  font-size: 4rem;
+  text-align: center;
+  color: white;
+  `;
+
+export default function App(props) {
+  const { getSmurf } = props;
+  
+  return (
+    <Wrapper>
+      {error ? props.error : null}
+      <Header>Welcome to Smurfville</Header>
+      <SmurfList/>
+      {props.smurf.map((smurf) => (
+        <Header key={smurf.id}>{smurf.name}</Header>
+      ))}
+      <SmurfForm/>
+    </Wrapper>
+  );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs
+  };
+};
+
+export default connect(null, { getInsult })(App);
