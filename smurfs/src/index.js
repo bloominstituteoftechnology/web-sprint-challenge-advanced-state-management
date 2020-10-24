ac21a6730b4b8ad5e2dbef0a24438798e2bec1dc
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-24 19:20:01 -0400

    Installed all dependencies and created a new branch

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger'
+import thunk from "redux-thunk"
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let store = createStore(applyMiddleware(thunk, logger))
+
+
+
+ReactDOM.render (
+<Provider store={store}>
+<App />
+</Provider>
+,document.getElementById("root"));
