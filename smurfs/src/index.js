bcbd0d1186c8c72fef1292677bc2ff884533ce9c
kai-blt <blaitong@gmail.com>
2020-11-06 08:14:33 -0800

    base project setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './redux/reducers'
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Setup Redux Store
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>    
+,
+document.getElementById("root"));
---
+++ smurfs/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+
+}
+
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+} 
