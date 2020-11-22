d54d4b57cb3f4a7818b51af05f63addc79cd4c47
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-11-22 12:27:13 -0800

    created store and wrapped app in provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore, applyMiddleware} from "redux"
+import {Provider} from "react-redux"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore()
+//need to add middleware and reducer still
+
+
+
+ReactDOM.render(<Provider store={store}>
+                <App />
+                </Provider>, document.getElementById("root"));
