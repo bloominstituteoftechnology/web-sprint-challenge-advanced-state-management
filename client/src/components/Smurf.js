368f072ef57f8751d7587cdc24b763ddb6ff7859
zempo <zelenkosolomon@gmail.com>
2020-12-11 10:09:42 -0800

    update SmurfDisplay

--- client/src/components/Smurf.js
+++ client/src/components/Smurf.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 class Smurf extends React.Component {
-    render() {
-        const { smurf } = this.props;
+  render() {
+    const { smurf } = this.props;
 
-        return(<div data-testid="smurf" className="card">
-        </div>);
-    }
+    return <div data-testid='smurf' className='card'></div>;
+  }
 }
 
 export default Smurf;
--- client/src/components/SmurfDisplay.js
+++ client/src/components/SmurfDisplay.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { findingSmurf } from "../actions";
+import Smurf from "./smurf";
 
-export class SmurfDisplay extends React.Component {
-    render() {
-        return(<div>
-            
-        </div>)
-    }
-}
+const SmurfDisplay = (props) => {
+  useEffect(() => {
+    props.findingSmurf();
+  }, []);
 
-export default SmurfDisplay;
+  useEffect(() => {
+    props.findingSmurf();
+  }, [props.isPosting]);
+
+  return (
+    <div>
+      <h2>Here are the Smurfs</h2>
+      <p>{props.error}</p>
+      {props.smurfs.map((smurf) => (
+        <div>
+          <Smurf smurf={smurf}></Smurf>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+    isPosting: state.isPosting,
+  };
+};
+export default connect(mapStateToProps, { findingSmurf })(SmurfDisplay);
 
 //Task List:
 //1. Import in all needed components and library methods.
