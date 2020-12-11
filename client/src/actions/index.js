6e1c2d6d17e3b58a19b86d5d9071b2e43598194a
zempo <zelenkosolomon@gmail.com>
2020-12-11 09:50:25 -0800

    add actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
+export const FOUND_SMURF = "FOUND_SMURF";
+export const SMURF_NOT_FOUND = "SMURF_NOT_FOUND";
+export const MAKE_SMURF = "MAKE_SMURF";
+export const SMURF_NOT_MADE = "SMURF_NOT_MADE";
+export const IS_LOADING = "IS_LOADING";
+export const IS_POSTING = "IS_POSTING";
 
-//Task List:
-//1. Add fetch smurfs action: 
-//              - fetch and return initial list of smurfs
-//              - dispatch actions that indicate if we are waiting for a server response
-//              - dispatch an error text action if an error is returned from the server
-//2. Add add smurf action:
-//              - dispatch an error text action if smurf data does not includes a name, nickname and position field
-//              - send a post request with the smurf as body to see if there is an error
-//              - dispatch add smurf action if request is successful
-//              - dispatch an error text action if an request returns an error
-//3. Add set error text action:
-//              - return action object setting error text
-//4. Any other actions you deem nessiary to complete application.
+export const findingSmurf = () => {
+  return (dispatch) => {
+    dispatch({ type: IS_LOADING });
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then((res) => {
+        dispatch({ type: FOUND_SMURF, payload: res.data });
+      })
+      .catch((err) => {
+        dispatch({ type: SMURF_NOT_FOUND, payload: err.message });
+      });
+  };
+};
+
+export const makingSmurf = (smurf) => {
+  return (dispatch) => {
+    dispatch({ type: IS_POSTING });
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then((res) => {
+        dispatch({ type: MAKE_SMURF });
+      })
+      .catch((err) => {
+        dispatch({ type: SMURF_NOT_MADE, payload: err.message });
+      });
+  };
+};
