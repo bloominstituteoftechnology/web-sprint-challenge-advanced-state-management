543fd5571844e0e6bfa9e08f1eb8d9a335a51ee2
zempo <zelenkosolomon@gmail.com>
2020-12-11 09:59:04 -0800

    integrate redux into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducers";
 
-const rootElement = document.getElementById("root");
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-
-//Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk middleware support.
-//3. Wrap the App component in a react-redux Provider element.
