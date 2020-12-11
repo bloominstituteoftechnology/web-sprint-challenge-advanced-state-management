2b194bf3a35160370a0752be4b670e2e813f28d4
zempo <zelenkosolomon@gmail.com>
2020-12-11 09:53:19 -0800

    add smurfy reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,21 +1,62 @@
+import {
+  FOUND_SMURF,
+  SMURF_NOT_FOUND,
+  MAKE_SMURF,
+  SMURF_NOT_MADE,
+  IS_POSTING,
+  IS_LOADING,
+} from "../actions";
 
-export const initialState = {
-}
+const initialState = {
+  smurf: {
+    name: "",
+    age: "",
+    height: "",
+    error: "",
+  },
+  smurfs: [],
+  isLoading: false,
+  isPosting: false,
+};
 
-const reducer = ()=>{
-}
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FOUND_SMURF:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false,
+      };
 
-export default reducer;
+    case SMURF_NOT_FOUND:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    case MAKE_SMURF:
+      return {
+        ...state,
+        isPosting: false,
+      };
+    case SMURF_NOT_MADE:
+      return {
+        ...state,
+        error: action.payload,
+        isPosting: false,
+      };
+    case IS_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case IS_POSTING:
+      return {
+        ...state,
+        isPosting: true,
+      };
 
-//Task List:
-//1. Add in the initialState needed to hold: 
-//      - an array of smurfs
-//      - a boolean indicating if the app is loading
-//      - error text
-//2. Setup your reducer to take the state and action as peremeters
-//3. Add in cases to your reducer to handle:
-//      - The start of an api call
-//      - The end of an api call
-//      - The adding a smurf to the smurf list when added into payload
-//      - Setting Error Text
-//      - Any other state changes you see as necessary
+    default:
+      return state;
+  }
+};
