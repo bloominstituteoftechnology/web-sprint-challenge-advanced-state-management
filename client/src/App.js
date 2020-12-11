4e2a848e4dc8a12a141fd8e10c6022222fccaead
zempo <zelenkosolomon@gmail.com>
2020-12-11 10:25:05 -0800

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 
-import AddForm from './components/AddForm';
-import SmurfDisplay from './components/SmurfDisplay';
+import AddForm from "./components/AddForm";
+import SmurfDisplay from "./components/SmurfDisplay";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-dark bg-primary">
-          <a className="navbar-brand">Smurf Village Database</a>
+      <div className='App'>
+        <nav className='navbar navbar-dark bg-primary'>
+          <a className='navbar-brand'>Smurf Village Database</a>
         </nav>
         <main>
-          <AddForm/>
-          <SmurfDisplay/>
+          <h1>Smurf Town</h1>
+          <p>Come for adventure. Stay because of Smurfette&trade;</p>
+          <br />
+          <AddForm />
+          <SmurfDisplay />
         </main>
       </div>
     );
