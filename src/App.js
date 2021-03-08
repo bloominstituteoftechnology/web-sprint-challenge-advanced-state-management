f5edcdc66cb0a3d71b91b4d8307fda5304b90a4b
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-03-07 21:22:29 -0700

    finished task list in app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from "react";
-
+import React, { useEffect } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
+import { fetchSmurfs, addSmurf } from './actions/index';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
+function App() {
 
-        <main>
-          <SmurfList/>
-          <AddForm/>
-        </main>
-      </div>
-    );
+  const dispatch = useDispatch();
+  const { smurf, loading } = useSelector((state) => state);
+
+  useEffect(() => {
+    fetchSmurfs(dispatch);
+  }, [dispatch]);
+
+  if (loading) {
+    return <><h2>Loading...</h2></>;
+  }
+
+  return (
+    <div className="App">
+      <Header />
+
+      <main>
+        <SmurfList smurf={smurf}/>
+        <AddForm/>
+      </main>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+    return {
+      smurfs: state.smurfs,
+      loading: state.loading,
+      error: state.error
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addSmurf: newSmurf => dispatch(addSmurf(newSmurf))
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
