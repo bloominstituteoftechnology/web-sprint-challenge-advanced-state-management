75a78d03110dc12bc3d8605fed82dc7732691af5
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-19 08:20:31 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import Provider from "react-redux"
 
 import "./index.css";
 import App from "./App";
