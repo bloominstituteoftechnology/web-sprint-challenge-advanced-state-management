b525e3113178ff54dbc4c3573e9046564347170b
Carson Myers <carsonscottmyers@gmail.com>
2021-03-19 11:52:55 -0600

    Carson Myers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import "./index.css";
 import App from "./App";
+import reducer from './reducers/index';
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-    <App />, 
-    rootElement
-);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
 
 //Task List:
 //1. Add in all necessary components and libary methods.
