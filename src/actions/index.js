a0fb06a150e26fc89e393e1ae3ce73d8237d871c
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-14 11:45:14 -0400

    added standard action to set value of the error message slice of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,6 +17,13 @@ export const fetchSmurfs = () => (dispatch) => {
     });
 };
 
+export const errorAction = (error) => {
+  return {
+    type: ERROR,
+    payload: error,
+  };
+};
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
