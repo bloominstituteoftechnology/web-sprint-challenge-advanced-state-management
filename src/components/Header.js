ad532f21ac0e3bbf29c0f8f12e32eced790b225e
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-05-14 09:44:11 -0400

    MVP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Header = ()=>{
-    return(<nav className="navbar navbar-dark bg-primary">
-        <a className="navbar-brand">Smurf Village Database</a>
+    return(
+    <nav className="navbar navbar-dark bg-primary">
+        <a href='/' className="navbar-brand">Smurf Village Database</a>
     </nav>);
 }
 
--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Smurf from './Smurf';
 
- const SmurfList = ()=> {
-    const isLoading = false;
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    }
+// imports
+import {connect} from 'react-redux';
+
+
+ const SmurfList = (props)=> {
+    const isLoading = props.loading;
+    // const testSmurf = {
+    //     id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+    //     name:'Poppa Smurf',
+    //     position:'Village Leader',
+    //     nickname: 'Pops',
+    //     description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
+    // }
 
     if (isLoading) {
         return <h1>Loading...</h1>;
     }
 
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+        {props.smurfArray.map(smurf => (<Smurf key ={smurf.id} smurf = {smurf}/>))}
+        {/* <Smurf smurf={testSmurf}/> */}
     </div>);
 }
+const mapStateToProps = (state) => {
+    return {
+        smurfArray: state.smurfArray,
+        loading: state.loading
+    }
+}
 
-export default SmurfList;
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
