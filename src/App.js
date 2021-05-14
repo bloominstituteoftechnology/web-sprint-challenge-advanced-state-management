0a649b7e5b508cf3a27ca83eea94bbb6c8e18e87
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-14 12:31:07 -0400

    component calls the fetchsmurf action on first render

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchSmurfs } from "./actions/index";
 
-import AddForm from './components/AddForm';
-import SmurfList from './components/SmurfList';
-import Header from './components/Header';
+import AddForm from "./components/AddForm";
+import SmurfList from "./components/SmurfList";
+import Header from "./components/Header";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
   render() {
     return (
       <div className="App">
         <Header />
 
         <main>
-          <SmurfList/>
-          <AddForm/>
+          <SmurfList />
+          <AddForm />
         </main>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, { fetchSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
