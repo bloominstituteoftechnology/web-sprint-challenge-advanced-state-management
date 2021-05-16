e301b1d397d07626133d935d697ab4221a38c6b0
artofmayhem <chef_tony_miller@yahoo.com>
2021-05-16 11:35:35 -1000

    test codegrade

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -70,4 +70,4 @@ export default connect(mapStateToProps, mapDispatchToProps)(AddForm)
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
 //2. Replace all instances of the errorMessage static variable with your error message state value. 
 //3. Within the handleSubmit function, replace the static assignment to errorMessage with a call to the setError action. Test that an error is displayed when this validation code fails.
-//4. Within the handleSubmit function, call your addSmurf action with the smurf name, position, nickname and summury passed as arguments. Test that a smurf is correctly added to when the form is submitted.
+//4. Within the handleSubmit function, call your addSmurf action with the smurf name, position, nickname and summary passed as arguments. Test that a smurf is correctly added to when the form is submitted.
