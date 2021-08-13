20b05312fd578c426af33b0a20f128f2a5c0604d
Rick <rmansfield@realpeoplesratings.com>
2021-08-13 10:18:24 -0500

    Index.js add components create store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
+//Task List:
+//1. Add in all necessary components and libary methods.
+import reducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { Provider } from "react-redux";
+
+//2. Create a store that includes thunk and logger middleware support.
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 const { worker } = require('./mocks/browser');
 worker.start();
@@ -14,7 +24,6 @@ ReactDOM.render(
     rootElement
 );
 
-//Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
+
+
 //3. Wrap the App component in a react-redux Provider element.
