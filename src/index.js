6d9123d852d40d8e30dcc2e51b7a5fcc72197243
Rick <rmansfield@realpeoplesratings.com>
2021-08-13 15:03:03 -0500

    Clean up and format index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
+//1. Add in all necessary components and libary methods.
 import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-
+import reducer from './reducers/index';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-
-import reducer from './reducers/index';
-
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./App";
 
+//2. Create a store that includes thunk and logger middleware support.
 const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 const { worker } = require('./mocks/browser');
@@ -18,6 +18,7 @@ worker.start();
 
 const rootElement = document.getElementById("root");
 
+//3. Wrap the App component in a react-redux Provider element.
 ReactDOM.render(
     <Provider store={store}>
         <App />
