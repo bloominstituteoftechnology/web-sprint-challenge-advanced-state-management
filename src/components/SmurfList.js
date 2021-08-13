91b4a0c13bb19b0ba1d7f7591c204fdd44ee809a
Rick <rmansfield@realpeoplesratings.com>
2021-08-13 15:16:31 -0500

    clean up and add breadcrumbs to SmurfList.js

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -2,8 +2,17 @@ import React from 'react';
 import Smurf from './Smurf';
 import { connect } from 'react-redux';
 
+    //3. Replace the static isLoading variable with the state loading variable.
  const SmurfList = (props)=> {
     const { smurfs, isLoading, dataError } = props;
+        // const isLoading = false;
+    // const testSmurf = {
+    //     id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+    //     name:'Poppa Smurf',
+    //     position:'Village Leader',
+    //     nickname: 'Pops',
+    //     description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
+    // }
 
     if (dataError ) {
         return <h2>Error retrieving data: {dataError}</h2>;
@@ -13,6 +22,7 @@ import { connect } from 'react-redux';
         return <h1>Loading...</h1>;
     }
     return(<div className="listContainer">
+              {/* 2. Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list. */}
         {smurfs.map((smurf, id) => (
                   <Smurf smurf={smurf}/>  
         ))}
@@ -25,7 +35,7 @@ const mapStateToProps = state => {
       dataError: state.dataError
     };
   };
-  
+//1. Connect the smurfs and loading state values to the SmurfList component.  
 export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
