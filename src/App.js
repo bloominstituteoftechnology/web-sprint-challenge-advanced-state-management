7d6fd4180142048eeeef7cd687ecb0b906b8e322
Rick <rmansfield@realpeoplesratings.com>
2021-08-13 15:25:23 -0500

    Additional BreadCrumbs in App.js

--- src/App.js
+++ src/App.js
@@ -40,3 +40,29 @@ export default connect(mapStateToProps)(App);
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
 //2. Call the fetchSmurfs action when the component first loads.
+
+//Could not get this verison to work. So I wrote the above function component instead. I'm leaving this here to try and work it out later. 
+/*
+
+class App extends Component {
+  //2. Call the fetchSmurfs action when the component first loads.
+  componentDidMount() {
+    this.props.fetchSmurfs()
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <main>
+          <SmurfList />
+          <AddForm />
+        </main>
+      </div>
+    );
+  }
+}
+
+//1. Connect the fetchSmurfs actions to the App component.
+export default connect(null, { fetchSmurfs })(App);
+*/
