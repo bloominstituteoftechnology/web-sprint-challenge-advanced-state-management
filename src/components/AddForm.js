92fd62e4c0520683ddbe6ed3819090f8d7e7e6c0
Rick <rmansfield@realpeoplesratings.com>
2021-08-13 15:23:13 -0500

    Clean, format, leave breadcrumbs in AddForm.js

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { addSmurf, setError } from "../actions"
 
 const initialValues = {
-    name:"",
-    position:"",
-    nickname:"",
-    description:""
+    name: "",
+    position: "",
+    nickname: "",
+    description: ""
 }
 const AddForm = (props) => {
     const [state, setState] = useState(initialValues);
@@ -14,10 +14,11 @@ const AddForm = (props) => {
     const handleChange = e => {
         setState({
             ...state,
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
         });
     }
-
+    //3. Within the handleSubmit function, replace the static assignment to errorMessage with a call to the setError action. Test that an error is displayed when this validation code fails.
+    //4. Within the handleSubmit function, call your addSmurf action with the smurf name, position, nickname and summury passed as arguments. Test that a smurf is correctly added to when the form is submitted.
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
@@ -25,7 +26,7 @@ const AddForm = (props) => {
         } else {
             props.setError("");
             setState(initialValues);
-            return( 
+            return (
                 props.addSmurf({
                     name: state.name,
                     position: state.position,
@@ -34,34 +35,42 @@ const AddForm = (props) => {
                 }));
         }
     }
-
-    return(<section>
+    //2. Replace all instances of the errorMessage static variable with your error message state value. 
+    //  const errorMessage = ""; //see new replacement code nest to the note to self below. 
+    return (<section>
         <h2>Add Smurf</h2>
         <form onSubmit={handleSubmit}>
             <div className="form-group">
-                <label htmlFor="name">Name:</label><br/>
+                <label htmlFor="name">Name:</label><br />
                 <input onChange={handleChange} value={state.name} name="name" id="name" />
             </div>
             <div className="form-group">
-                <label htmlFor="position">Position:</label><br/>
+                <label htmlFor="position">Position:</label><br />
                 <input onChange={handleChange} value={state.position} name="position" id="position" />
             </div>
             <div className="form-group">
-                <label htmlFor="nickname">Nickname:</label><br/>
+                <label htmlFor="nickname">Nickname:</label><br />
                 <input onChange={handleChange} value={state.nickname} name="nickname" id="nickname" />
             </div>
             <div className="form-group">
-                <label htmlFor="description">Description:</label><br/>
+                <label htmlFor="description">Description:</label><br />
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
                 props.errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.errorMessage}</div>
             }
+            {/* note to self. Need more work here - had to create a function component in App.js Could not get this error to work with the Class Component. Super disappointed */}
             <button>Submit Smurf</button>
         </form>
     </section>);
 }
-const mapStateToProps = (state)=> {
+//Simply could not get this block to work with the Class Compoenent. I'm leaving it here to try to work out later. 
+// const mapStateToProps = state => {
+//     return {
+//         error: state.error
+//     }
+// }
+const mapStateToProps = (state) => {
     return {
         name: state.name,
         position: state.position,
@@ -69,10 +78,11 @@ const mapStateToProps = (state)=> {
         description: state.description,
         errorMessage: state.errorMessage
     }
-  }
-  
+}
+//1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
+//   export default AddForm; this code replaced with the line below
 export default connect(mapStateToProps, { addSmurf, setError })(AddForm);
-  
+
 
 
 //Task List:
