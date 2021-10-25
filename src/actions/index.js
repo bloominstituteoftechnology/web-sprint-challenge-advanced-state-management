7d99e06cd44905a65e9aa36ecfa0cbbcee959bf4
LirongCui <lirongcoding@gmail.com>
2021-10-24 21:55:40 -0700

    working

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,17 +6,19 @@ export const ERROR = 'ERROR';
 export const ADD_SMURF = 'ADD_SMURF';
 export const SET_ERROR = 'SET_ERROR';
 
-export const fetchSmurfs = (dispatch) => {
-    dispatch({type: START_FETCH})
-    axios.get('http://localhost:3333/smurfs')
-        .then((res) => {
-            console.log(res)
-            dispatch({ type:'FETCH_SUCCESS', payload: res.data});
-        })
-        .catch((err) => {
-            console.log({err})
-            dispatch({ type:'ERROR', payload: err.message})
-        })
+export const fetchSmurfs = () => {
+    return (dispatch) => {
+        dispatch({type: START_FETCH})
+        axios.get('http://localhost:3333/smurfs')
+            .then((res) => {
+                console.log(res)
+                dispatch({ type:'FETCH_SUCCESS', payload: res.data});
+            })
+            .catch((err) => {
+                console.log({err})
+                dispatch({ type:'ERROR', payload: err.message})
+            })
+    }
 };
 
 export const addSmurf = (smurf) => {
--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -8,21 +8,9 @@ import { fetchSmurfs } from '../actions';
     useEffect(() => {
         props.fetchSmurfs();
     },[]);
-    // const isLoading = false;
-    // const testSmurf = {
-    //     id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-    //     name:'Poppa Smurf',
-    //     position:'Village Leader',
-    //     nickname: 'Pops',
-    //     description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    // }
-
-    // if (isLoading) {
-    //     return <h1>Loading...</h1>;
-    // }
-
+    
     return(<div className="listContainer">
-        {props.fetchSmurfs.map(smurf => <Smurf smurf={smurf} key={smurf.id}/>)}
+        {props.smurfs.map(smurf => <Smurf smurf={smurf} key={smurf.id}/>)}
     </div>);
 }
 
